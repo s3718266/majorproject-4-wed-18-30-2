b5856aa5a4bd1ede0e8a531dbd272e9367bddd55
Aleks <aleks@pixelstarships.com>
2020-08-19 18:23:06 +1000

    Major project backend commit with Spring Initializr

---
+++ Sprint1/BackEnd/src/main/java/com/wed18302/majorproject/MajorprojectApplication.java
@@ -0,0 +1,13 @@
+package com.wed18302.majorproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MajorprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MajorprojectApplication.class, args);
+	}
+
+}
---
+++ Sprint1/BackEnd/src/test/java/com/wed18302/majorproject/MajorprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wed18302.majorproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MajorprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
