e62c1d9fbe5f39674e9b07773b759134093fdb7d
Aleks <aleks@pixelstarships.com>
2020-09-11 18:37:01 +1000

    Accidentally had a 0

--- BackEnd/src/main/java/com/wed18302/majorproject/MajorProject.java
+++ BackEnd/src/main/java/com/wed18302/majorproject/MajorProject.java
@@ -36,7 +36,7 @@ public class MajorProject {
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**").allowedOrigins("http://localhost:3000");
  			}
-0	 	};
+	 	};
 	}
 	
 	
