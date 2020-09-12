94fb734dc46afa183a61c0594009565dd13238ba
AldoIrvine111 <aldoirvine@gmail.com>
2020-09-12 18:28:05 +1000

    User is added to backend

--- FrontEnd/agme/src/pages/sign-up.js
+++ FrontEnd/agme/src/pages/sign-up.js
@@ -2,70 +2,107 @@ import React from 'react';
 import '../App.css';
 import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-function SignUp() {
-  return (
-    <Form className="signup-form">
-      <h1 class="font-weight-bold" id="heading">Sign Up</h1>
-
-      <FormGroup>
-        <Label>First Name</Label>
-        <Input type="text" placeholder="First Name"></Input>
-      </FormGroup>
-
-      <FormGroup>
-        <Label>Last Name</Label>
-        <Input type="text" placeholder="Last Name"></Input>
-      </FormGroup>
-
-      <FormGroup>
-        <Label>Email</Label>
-        <Input type="email" placeholder="Email"></Input>
-      </FormGroup>
-
-      <FormGroup>
-        <Label>Contact Number</Label>
-        <Input type="text" placeholder="Contact Number"></Input>
-      </FormGroup>
-
-      <FormGroup>
-        <Label>Address</Label>
-        <Input type="text" placeholder="Address"></Input>
-      </FormGroup>
-
-      <FormGroup>
-        <Label>Gender</Label>
-        <br />
-        <select id="gender" name="gender">
-          <option value="Male">Male</option>
-          <option value="Female">Female</option>
-          <option value="Potato">Potato</option>
-        </select>
-      </FormGroup>
-
-      <FormGroup>
-        <Label>Date of Birth</Label>
-        <Input type="date" placeholder="DOB"></Input>
-      </FormGroup>
-
-      <FormGroup>
-        <Label>Password</Label>
-        <Input type="password" placeholder="Password"></Input>
-      </FormGroup>
-
-      <Button className="btn-lg btn-success btn-block mt-5 mb-3">
-        Log In
-      </Button>
-
-      <div className="text-center">
-        <a href="/sign-in">Sign up</a>
-        <span className="p-2">|</span>
-        <a href="/">Forgot Password</a>
-      </div>
+import config from '../Constants';
 
 
-    </Form>
-  );
+class SignUp extends React.Component {
+
+  constructor() {
+    super();
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event) {
+
+    event.preventDefault();
+    const data = new URLSearchParams(new FormData(event.target));
+
+    fetch(config.APP_URL + 'auth/register', {
+      method: 'POST',
+      body: data
+    })
+      .then(res => console.log(res))
+
+  }
+
+
+  render() {
+
+
+    return (
+      <Form className="signup-form" onSubmit={this.handleSubmit}>
+        <h1 className="font-weight-bold" id="heading">Sign Up</h1>
+
+        <FormGroup>
+          <Label>First Name</Label>
+          <Input type="text" name="firstname" placeholder="First Name" ref={node => (this.firstname = node)}></Input>
+        </FormGroup>
+
+        <FormGroup>
+          <Label>Last Name</Label>
+          <Input type="passtextword" name="lastname" placeholder="Last Name" ref={node => (this.lastname = node)}></Input>
+        </FormGroup>
+
+        <FormGroup>
+          <Label>Email</Label>
+          <Input type="email" name="email" placeholder="Email" ref={node => (this.email = node)}></Input>
+        </FormGroup>
+
+        <FormGroup>
+          <Label>Password</Label>
+          <Input type="password" name="password" placeholder="Password" ref={node => (this.password = node)}></Input>
+        </FormGroup>
+
+        {/*Do not forget to add this feature below after registration completed  */}
+        {/* <FormGroup>
+          <Label>Contact Number</Label>
+          <Input type="text" placeholder="Contact Number"></Input>
+        </FormGroup>
+  
+        <FormGroup>
+          <Label>Address</Label>
+          <Input type="text" placeholder="Address"></Input>
+        </FormGroup>
+  
+        <FormGroup>
+          <Label>Gender</Label>
+          <br />
+          <select id="gender" name="gender">
+            <option value="Male">Male</option>
+            <option value="Female">Female</option>
+            <option value="Potato">Potato</option>
+          </select>
+        </FormGroup>
+  
+        <FormGroup>
+          <Label>Date of Birth</Label>
+          <Input type="date" placeholder="DOB"></Input>
+        </FormGroup> */}
+
+
+
+        {/* TODO : Add confirm password feature */}
+        {/* <FormGroup>
+          <Label>Confirm Password</Label>
+          <Input type="password" placeholder="Confirm Password"></Input>
+        </FormGroup> */}
+
+        <Button className="btn-lg btn-success btn-block mt-5 mb-3" type="submit">
+          Sign Up
+        </Button>
+
+        <div className="text-center">
+          <a href="/sign-in">Sign up</a>
+          <span className="p-2">|</span>
+          <a href="/">Forgot Password</a>
+        </div>
+
+
+      </Form>
+    );
+
+  }
+
 }
 
 export default SignUp;
