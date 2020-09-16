6ae7993b1406c7358c158d90e81e0574fd5a1efb
Aleks <aleks@pixelstarships.com>
2020-09-16 18:39:11 +1000

    New tests and test profile

--- BackEnd/src/main/java/com/wed18302/majorproject/MajorProject.java
+++ BackEnd/src/main/java/com/wed18302/majorproject/MajorProject.java
@@ -1,19 +1,15 @@
 package com.wed18302.majorproject;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.error.ErrorAttributes;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class MajorProject {
-
-	@Autowired
-	private ErrorAttributes errorAttributes;
 	 
 	public static void main(String[] args) {
 		SpringApplication.run(MajorProject.class, args);
--- BackEnd/src/main/java/com/wed18302/majorproject/tests/UserTest.java
+++ BackEnd/src/main/java/com/wed18302/majorproject/tests/UserTest.java
@@ -7,15 +7,21 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.web.servlet.error.ErrorAttributes;
+import org.springframework.context.annotation.Profile;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 import com.wed18302.majorproject.model.User;
 import com.wed18302.majorproject.model.UserRepository;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
+@AutoConfigureWebTestClient
+@Profile("test")
 public class UserTest {
-    
+	
     @Autowired
     private UserRepository userRepository;
    
@@ -29,5 +35,45 @@ public class UserTest {
         var resultUser = userSearch.get(0);
         Assert.assertTrue(resultUser.equals(testUser));
     }    
+
+    @Test
+    public void registration_testDatabaseUserSearch() {
+    	registration_testDatabaseUserAddition();
+    	
+    	String search = "paul.smith@gmail.com";
+        
+        User resultUser = null;
+        for (User user : userRepository.findAll()) {
+        	if (user.getEmail() == search) {
+        		resultUser = user;
+        		break;
+        	}
+        }
+        
+        Assert.assertTrue(resultUser.getEmail() == search);
+    }    
+    
+    @Test
+    public void registration_testDatabaseUserDelete() {
+    	registration_testDatabaseUserAddition();
+    	
+    	String account = "paul.smith@gmail.com";
+    	
+        User resultUser = null;
+        for (User user : userRepository.findAll()) {
+        	if (user.getEmail() == user.getEmail()) {
+        		resultUser = user;
+        		break;
+        	}
+        }
+
+        Assert.assertTrue(resultUser != null);
+        Assert.assertTrue(userRepository.findByEMAIL(account) != null);
+        userRepository.delete(resultUser);
+        Assert.assertTrue(userRepository.findByEMAIL(account) == null);
+
+    }   
+    
+    
     
 }
