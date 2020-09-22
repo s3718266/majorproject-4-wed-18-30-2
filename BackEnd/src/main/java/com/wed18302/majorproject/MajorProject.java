e914edbf19809f1a7515311a38d182e3532ba1b1
Aleks <aleks@pixelstarships.com>
2020-09-22 17:56:44 +1000

    CORS

--- BackEnd/src/main/java/com/wed18302/majorproject/MajorProject.java
+++ BackEnd/src/main/java/com/wed18302/majorproject/MajorProject.java
@@ -46,5 +46,15 @@ public class MajorProject {
 		return serviceManager;
 	}
 	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+ 			}
+	 	};
+	}
+	
 	
 }
