1dfac9dbf966f5862a061eab7e4bb741c80fd5ef
NegarFarshchi <41820778+NegarFarshchi@users.noreply.github.com>
2020-09-25 20:54:01 +1000

    Update dashboard.js

--- FrontEnd/agme/src/pages/dashboard.js
+++ FrontEnd/agme/src/pages/dashboard.js
@@ -40,7 +40,6 @@ class Dashboard extends React.Component {
     window.datas = parsedData;
 
     this.renderTableData();
-    this.renderWorkers();
 
   }
 
@@ -72,29 +71,100 @@ class Dashboard extends React.Component {
 
     window.selectedService = select.options[select.selectedIndex];
 
-  }
-
-  renderWorkers() {
+    document.getElementById('worker').innerHTML = "";
+    window.selectedWorker = "";
 
     for (var k of Object.values(window.datas)) {
 
-      for (var g of Object.values(k.workers)) {
+      if (k.id == window.selectedService.id) {
 
-        document.getElementById('service').innerHTML += "<option id='" + g.id + "'>" + g.firstName + "</option>";
+        for (var g of Object.values(k.workers)) {
 
-      };
+          if (window.selectedWorker == "") {
+            window.selectedWorker = g.id;
+          }    
+
+          document.getElementById('worker').innerHTML += "<option id='" + g.id + "'>" + g.firstName + "</option>";
+
+        };
+
+      }
 
     };
 
   }
 
+  handleWorker(e) {
+
+    var select = document.getElementById('worker');
+
+    window.selectedWorker = select.options[select.selectedIndex];
+
+  }
+
   componentWillMount() {
     this.getServices();
+    this.getUserId();
+  }
+
+  getUserId() {
+
+    const data = encodeURI('auth-token=' + localStorage.getItem('auth_token'));    
+
+    fetch(config.APP_URL + 'auth/getuser', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: data
+    })
+      .then(res => res.json())
+      .then(res => {
+
+        for (var k in res) {
+          if (res.hasOwnProperty(k)) {
+
+            if(res[k]['id']) {
+              localStorage.setItem('user_id', res[k]['id']);
+            }
+
+          }
+        }
+
+      });
+
+  }
+
+  handleSubmit(event) {
+
+    event.preventDefault();
+
+    var datetime = document.getElementById('datetime').value;
+    var my_user_id = localStorage.getItem('user_id');
+    var worker_id = window.selectedWorker;
+
+    const data = encodeURI('service-id=' + window.selectedService.id + '&booking-date=' + datetime + "&customer-id=" + my_user_id + "&worker-id=" + worker_id);
+
+    fetch(config.APP_URL + 'booking/create', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: data
+    })
+      .then(res => res.json())
+      .then(res => {
+
+        console.log(res);
+
+      });
+
   }
 
   render() {
 
     return (
+
       <div class="container">
         <h1 id='title'>List of services</h1>
         <table id='datas'>
@@ -106,28 +176,29 @@ class Dashboard extends React.Component {
             </tr>
           </tbody>
         </table>
+
         <br />
 
-        <Form className="login-form">
+        <Form className="" onSubmit={this.handleSubmit}>
 
           <div className="alert alert-danger d-none" id="errorMessage">
           </div>
 
           <FormGroup>
             <Label>Date and Time</Label>
-            <Input type="date" name="datetime" placeholder="Date and Time"></Input>
+            <Input type="date" id="datetime" name="datetime" placeholder="Date and Time"></Input>
           </FormGroup>
 
           <FormGroup>
             <Label for="service">Services</Label>
             <Input type="select" name="service" id="service" onChange={this.handleService}>
-
+              <option value=""></option>
             </Input>
           </FormGroup>
 
           <FormGroup>
             <Label for="worker">Worker</Label>
-            <Input type="select" name="worker" id="worker">
+            <Input type="select" name="worker" id="worker" onChange={this.handleWorker}>
 
             </Input>
           </FormGroup>
